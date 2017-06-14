1d700b5ef9ce19c11c44a89705d85ae11a9757f7
JuanD <juan.d.prado@correounivalle.edu.co>
2017-06-13 19:53:22 -0500

    Finished HeroDetailComponent call in appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ const HEROES: Hero[] = [
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
+  <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styles: [`
     .selected {
